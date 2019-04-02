d1fbc7a5477a4dea60de95f7e4a0b69a62631c0d
leochepigo <48070980+leochepigo@users.noreply.github.com>
2019-04-02 21:24:37 +0300

    hw3

--- hw_03/matrix.h
+++ hw_03/matrix.h
@@ -4,15 +4,11 @@
 class Row {
 	size_t ncols;
 	int* data;
-
 public:
-	Row(): ncols(0), data(nullptr) {};
-	Row(size_t _ncols): ncols(_ncols), data(new int[_ncols]) {};
-	~Row() { delete[] data;	};
+	Row(int* _data, size_t _ncols): ncols(_ncols), data(_data) {};
 
-	int& operator [] (size_t n);
-	const int& operator [] (size_t n) const;
-	Row& operator = (const Row& row);
+	int& operator[] (size_t n);
+	int operator[] (size_t n) const;
 };
 
 int& Row::operator[] (size_t n) {
@@ -22,73 +18,56 @@ int& Row::operator[] (size_t n) {
 	else throw std::out_of_range("");
 }
 
-const int& Row::operator[] (size_t n) const {
+int Row::operator[] (size_t n) const {
 	if (n < ncols) {
 		return data[n];
 	}
 	else throw std::out_of_range("");
 }
 
-Row& Row::operator = (const Row& row) {
-	ncols = row.ncols;
-	delete[] data;
-	data = new int [row.ncols];
-	for (size_t i = 0; i<row.ncols; ++i) data[i] = row.data[i];
-	return *this;
-}
-
 class Matrix {
-	const size_t nrows;
-	const size_t ncols;
-	Row* rows;
+	size_t nrows;
+	size_t ncols;
+	int* data;
 
 public:
-	Matrix(size_t _nrows, size_t _ncols): nrows(_nrows), ncols(_ncols), rows(new Row[_nrows]) {
-		Row temp = Row(_ncols);
-		for (size_t i =0; i<_nrows; ++i){
-			rows[i] = temp;
-		}
-	};
-	~Matrix() {	delete[] rows; }
+	Matrix(size_t _nrows, size_t _ncols): nrows(_nrows), ncols(_ncols), data(new int[_nrows*_ncols]) { };
+	~Matrix() {	delete[] data; };
 
-	Row& operator[] (size_t n);
-	const Row& operator[] (size_t n) const;
+	Row operator[] (size_t n);
+	const Row operator[] (size_t n) const;
 	Matrix& operator *= (int n);
 	bool operator == (const Matrix& m) const;
 	bool operator != (const Matrix& m) const;
-	size_t getRows() const { return nrows; }
-	size_t getColumns() const { return ncols; }
+	size_t getRows() const { return nrows; };
+	size_t getColumns() const { return ncols; };
 };
 
-Row& Matrix::operator[] (size_t n) {
+Row Matrix::operator[] (size_t n) {
 	if (n < nrows) {
-		return rows[n];
+		return Row(data + n*ncols, ncols);
 	}
 	else throw std::out_of_range("");
 }
 
-const Row& Matrix::operator[] (size_t n) const {
+const Row Matrix::operator[] (size_t n) const {
 	if (n < nrows) {
-		return rows[n];
+		return Row(data+n*ncols, ncols);
 	}
 	else throw std::out_of_range("");
 }
 
 Matrix& Matrix::operator *= (int n) {
-	for (size_t i = 0; i < nrows; ++i) {
-		for (size_t j = 0; j < ncols; ++j) {
-			rows[i][j] *= n;
-		}
+	for (size_t i = 0; i < nrows*ncols; ++i) {
+		data[i]*=n;
 	}
 	return *this;
 }
 
 bool Matrix::operator == (const Matrix& m) const {
 	if (ncols == m.ncols && nrows == m.nrows) {
-		for (size_t i = 0; i < nrows; ++i) {
-			for (size_t j = 0; j < ncols; ++j) {
-				if (rows[i][j] != m[i][j]) return false;
-			}
+		for (size_t i = 0; i < nrows*ncols; ++i) {
+			if (data[i] != m.data[i]) return false;
 		}
 	}
 	else return false;
