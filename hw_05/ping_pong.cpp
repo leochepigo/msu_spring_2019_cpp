53e305fbc746d242be0313408e037727d67b2d27
leochepigo <48070980+leochepigo@users.noreply.github.com>
2019-04-20 11:34:52 +0300

    hw5

---
+++ hw_05/ping_pong.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <thread>
+#include <mutex>
+#include <condition_variable>
+
+using namespace std;
+
+static mutex m;
+static condition_variable c;
+static constexpr size_t Nmax = 500000;
+
+void tabble_tennis(string data)
+{
+	for (size_t N = 0; N < Nmax; ++N)
+    {
+		unique_lock<std::mutex> lock(m);
+        cout << data;
+        c.notify_one();
+        c.wait(lock);
+    }
+
+    c.notify_one();
+}
+
+int main()
+{
+	thread t1(tabble_tennis,"ping\n");
+	thread t2(tabble_tennis,"pong\n");
+
+	t1.join();
+	t2.join();
+
+    return 0;
+}
